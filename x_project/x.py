40132cfea0c8ae4218284b7cf7828f1811a7b4b2
Kateřina Chmelařová <chmelarova.kata@gmail.com>
2023-12-26 17:39:21 +0100

    works with Brave browser, shows multiple houses and correct links together with some additional links and some links are depicted multiple times.

--- x_project/x.py
+++ x_project/x.py
@@ -1,4 +1,3 @@
-
 url = "https://www.sreality.cz/hledani/prodej/domy/kralovehradecky-kraj,pardubicky-kraj?region=m%C4%9Bstsk%C3%A1%20%C4%8D%C3%A1st%20B%C4%9Ble%C4%8Dko&no_shares=1&region-id=148&region-typ=ward&bez-aukce=1&vzdalenost=5#z=12"
 
 import sqlite3
@@ -9,9 +8,18 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 
+
+#brave
+BROWSER_PATH = r"/Applications/Brave Browser.app/Contents/MacOS/Brave Browser"
+OPTIONS = webdriver.ChromeOptions()
+OPTIONS.binary_location = BROWSER_PATH
+OPTIONS.add_experimental_option("detach", True)
+
+driver=webdriver.Chrome(options=OPTIONS)
+
 # webdriver, další 2 řádky must have 
 # mám odsud https://stackoverflow.com/questions/76550506/typeerror-webdriver-init-got-an-unexpected-keyword-argument-executable-p
-driver=webdriver.Chrome()
+# tohle přidat - driver=webdriver.Chrome()
 driver.get(url) 
 
 # uložení objektu do proměnné response
@@ -43,22 +51,21 @@ try:
     time.sleep(2.5)
     driver.refresh()
 
-    #hledá všechny 'a'
-    links = soup.find_all('a')  
+    soup.prettify()
     
-    #soup.prettify()
+    #hledá všechny 'a'
+    #links = soup.find_all('a')  
 
-    for link in links:
-        print(link.get('href'))
+    #for link in links:
+        #print(link.get('href'))
     
     elements = driver.find_elements(By.TAG_NAME, 'a')  # You can change 'a' to another tag if needed
 
     for element in elements:
-        href = element.get_attribute('href')
-        print(href)
+        odkaz = element.get_attribute('href')
+        print(odkaz)
 
     
-
 finally:
     connection.commit()
     connection.close()
